d6fd6d420e42d3668b105a2166fbac13d0e066a4
Jamie Grafton <jamiegrafton@gmail.com>
2018-04-01 12:53:23 -0400

    wrote hit and miss functions in js

---
+++ main.js
@@ -0,0 +1,15 @@
+let view = {
+    displayMessage: function(msg) {
+        let messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+
+    },
+    displayMiss: function(location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+};

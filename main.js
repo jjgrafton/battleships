c9a0f657baf15cd10abdeeb5792be2313f5fa81e
Jamie Grafton <jamiegrafton@gmail.com>
2018-04-01 14:01:48 -0400

    added controller and parseGuess functions

--- main.js
+++ main.js
@@ -58,3 +58,42 @@ let model = {
         return true;
     }
 }
+
+let controller = {
+    guesses: 0,
+
+    processGuess: function(guess) {
+        let location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            let hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+            }
+        }
+
+
+    }
+};
+
+function parseGuess (guess) {
+    let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board");
+    } else {
+        // grab the first character of the guess
+        let firstChar = guess.charAt(0);
+        let row = alphabet.indexOf(firstChar);
+        let column = guess.charAt[1];
+
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board");
+        } else if (row < 0 || row>= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert("Oops, that's off the board!");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+};

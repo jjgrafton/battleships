27eda157ba9bb29acf9d90b975501e03642e0daa
Jamie Grafton <jamiegrafton@gmail.com>
2018-04-01 13:22:44 -0400

    created fire function to determine, but iterating through arrays, whether a ship has been hit

--- main.js
+++ main.js
@@ -22,6 +22,22 @@ let model = {
 
     ships: [{locations: ["06", "16", "26"], hits: ["", "", ""]},
             {locations: ["24", "34", "44"], hits: ["", "", "",]},
-            {locations: ["10", "11", "12"], hits: ["", "", "",]}
-]
+            {locations: ["10", "11", "12"], hits: ["", "", "",]}],
+
+    fire: function(guess) {
+//iterate through the array of ships, examining one at a time
+        for (let i = 0; i < this.numShips; i++) {
+            //Does the guess match any od the ship's locations?
+            let ship = this.ships[i];
+            let locations = ship.locations;
+            let index = locations.indexOf(guess);
+            // the indexOf method searches an array for a matching value, and returns its index or -1 if it can't find it
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
